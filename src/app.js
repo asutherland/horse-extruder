de626f3382c5a83bd239ceefa284912a2f64118c
Andrew Sutherland <asutherland@asutherland.org>
2014-02-14 23:30:30 -0500

    ThreeCSG demo variant

--- src/app.js
+++ src/app.js
@@ -1,25 +1,40 @@
 define(function(require, exports, module) {
 var THREE = require('three');
+var ThreeBSP = require('threeBSP');
 
 exports.main = function() {
-  var camera, scene, renderer;
-  var geometry, material, mesh;
+  var camera, scene, renderer, funkyMesh
 
   init();
   animate();
 
   function init() {
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera = new THREE.PerspectiveCamera(
+               75,
+               window.innerWidth / window.innerHeight, 1, 10000 );
     camera.position.z = 1000;
 
     scene = new THREE.Scene();
 
-    geometry = new THREE.CubeGeometry( 200, 200, 200 );
-    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+    var cubeGeom = new THREE.CubeGeometry( 200, 200, 200 );
+    var cubeMesh = new THREE.Mesh(cubeGeom);
+    var cubeBSP = new ThreeBSP(cubeMesh);
 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    var sphereGeom = new THREE.SphereGeometry( 100, 32, 32 );
+    var sphereMesh = new THREE.Mesh(sphereGeom);
+    sphereMesh.position.set(100, 100, 100);
+    var sphereBSP = new ThreeBSP(sphereMesh);
+
+    var funkyBSP = cubeBSP.subtract(sphereBSP);
+    var funkyMaterial =  new THREE.MeshBasicMaterial({
+      color: 0xff0000,
+      wireframe: true
+    });
+    funkyMesh = funkyBSP.toMesh(funkyMaterial);
+    funkyMesh.geometry.computeVertexNormals();
+
+    scene.add( funkyMesh );
 
     renderer = new THREE.CanvasRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -32,8 +47,8 @@ exports.main = function() {
     // note: three.js includes requestAnimationFrame shim
     requestAnimationFrame( animate );
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
+    funkyMesh.rotation.x += 0.01;
+    funkyMesh.rotation.y += 0.02;
 
     renderer.render( scene, camera );
   }
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,10 +2,15 @@ require.config({
   baseUrl: 'src',
   paths: {
     'three': '../lib/three.min',
+    'threeBSP': '../lib/ThreeCSG',
   },
   shim: {
     'three': {
       exports: 'THREE'
+    },
+    'threeBSP': {
+      deps: ['three'],
+      exports: 'ThreeBSP'
     }
   }
 });
